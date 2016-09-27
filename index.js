773bc43d24670f8615c314c509bdc79e8b1f6146
Cedric David <cedric.david@btopenworld.com>
2016-09-27 02:15:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function iterativeLog(array){
+  array.forEach(function
+    other(element, index, array) {
+
+    console.log(`${index}: ${element}`);
+  })
+;}
+
+
+
+function iterate(callback){
+var numbers = [1,2,3];
+  numbers.forEach(callback);
+return numbers;
+
+  }
+
+function doToArray(array,callback){
+  array.forEach(callback);
+}
